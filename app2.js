7fcb91095a9e6f70adedeb4a0a0b9278cbbb1d5a
Om Raja <omraja451@gmail.com>
2024-12-25 23:50:21 +0530

    learnt functional component, component composition and Element inside component etc

--- app2.js
+++ app2.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// *! JSX
 //Using JSX from now on. Because React.createElement() is not efficient.
 //JSX is converted to React.createElement() by Babel.
 //JSX is a syntactic sugar for React.createElement()
@@ -14,7 +15,49 @@ import ReactDOM from "react-dom/client";
 //JSX is a syntax extension for JavaScript.
 //JSX is used with React to describe what the UI should look like.
 
-//JSX
 var jsxHeading = <h1 id="heading">Namaste React from JSX</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxHeading);
+
+/** REACT COMPONENTS
+ * 1. Functional Components (New)
+ * 2. Class Components (Old)
+ * ! class component is old now. Functional components are used now.
+ * We will still learn class based components because there are many old projects that use class based components. And there are still some interview questions on class based components.
+ */
+
+// * A function which returns JSX is called functional component.
+// *! variable name start with capital letter
+const HeadingComponent = () => {
+  return <h1 className="headings">Namaste React from JSX</h1>;
+};
+
+const HeadingComponent2 = () => (
+  <h1 className="headings">Namaste React from JSX</h1>
+);
+
+// * Render a functional component
+const secondRoot = ReactDOM.createRoot(document.getElementById("secondRoot"));
+const thirdRoot = ReactDOM.createRoot(document.getElementById("thirdRoot"));
+secondRoot.render(<HeadingComponent />);
+thirdRoot.render(<HeadingComponent2 />);
+
+const ExerciseComponent = () => {
+  return (
+    <div>
+      <p>Hi This is Om Raja from Functional component</p>
+      {jsxHeading}
+    </div>
+  );
+};
+const CompositionComponent = () => {
+  return (
+    <div>
+      <ExerciseComponent />
+      <h2>Heading second by composition component</h2>
+    </div>
+  );
+};
+
+const fourthRoot = ReactDOM.createRoot(document.getElementById("fourthRoot"));
+fourthRoot.render(<CompositionComponent />);
