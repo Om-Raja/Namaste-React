186a3a162cb531e3097dbaa3c3d2d98c91bd64f1
Om Raja <omraja451@gmail.com>
2025-01-04 12:37:19 +0530

    learnt Shimmer UI, useEffect()

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,33 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 
 const Body = () => {
+  //state variable
   const [listOfRestaurants, setListOfRestaurants] = useState(restaurantList);
-  return (
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  const fetchData = async ()=>{
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.900965&lng=75.8572758&collection=83637&tags=layout_CCS_Burger&sortBy=&filters=&type=rcv2&offset=0&page_type=null");
+
+    const json = await data.json();
+    console.log(json.data.cards[2].card.card.info);
+
+    // setListOfRestaurants(json);
+  };
+
+  // conditional rendering - A rendering based on some condition.
+ /* if(listOfRestaurants.length === 0){
+    return (<h1>Loading...</h1>); // this is a bad way for UX that's why we use shimmer UI
+  } */ 
+ //replacing above code by ternary operator below with return keyword
+  
+  return (listOfRestaurants.length === 0)? <Shimmer/> : (
     <div id="body">
       <div id="search_bar"></div>
       <button
